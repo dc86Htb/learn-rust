0ed30987b30de3ea6731f155283c6088b0f8dd5b
Willump <93250255+dc86Htb@users.noreply.github.com>
2022-07-04 00:01:04 +0200

    first rust program !

--- main.rs
+++ main.rs
@@ -1,16 +1,37 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
     println!("Devinez le nombre !");
 
-    println!("Veuillez entrer un nombre.");
+    let nombre_secret = rand::thread_rng().gen_range(1..101);
 
-    let mut supposition = String::new();
+    loop {
+        println!("Veuillez entrer un nombre.");
 
-    io::stdin()
-        .read_line(&mut supposition)
-        .expect("Échec de la lecture de l'entrée utilisateur");
+        let mut supposition = String::new();
 
-    println!("Votre nombre : {}", supposition);
+        io::stdin()
+            .read_line(&mut supposition)
+            .expect("Échec de la lecture de l'entrée utilisateur");
+
+        let supposition: u32 = match supposition.trim().parse() {
+            Ok(nombre) => nombre,
+            Err(_) => continue,
+        };
+
+        println!("Votre nombre : {}", supposition);
+
+        match supposition.cmp(&nombre_secret) {
+            Ordering::Less => println!("C'est plus !"),
+            Ordering::Greater => println!("C'est moins !"),
+            Ordering::Equal => {
+                println!("Vous avez gagné !");
+                break;
+            }
+        }
+    }
 }
+
 
